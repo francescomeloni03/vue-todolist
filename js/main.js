09e86f182c5ad8c39392d688627cbb8c269a8148
Francesco Meloni <francesco.meloni03@icloud.com>
2023-05-10 18:59:58 +0200

    Milestone 1/2/3(n.f)

---
+++ js/main.js
@@ -0,0 +1,54 @@
+const { createApp } = Vue
+
+createApp({
+  data() {
+    return {
+        newtodo: "",
+        todoList: [
+        {
+            text:`Lavare i piatti`,
+            done: true,
+        },
+
+        {
+            text:`Lavare la macchina`,
+            done: false,  
+        },
+
+        {          
+            text:`Stendere i panni`,
+            done: true,
+        },
+
+        { 
+            text:`Fare la spesa`,
+            done: false,
+        },
+
+        {
+            text:`Pulire casa`,
+            done: true ,
+        },
+      ]
+  
+    }
+  },
+  methods: {
+      addToDo(){
+        this.todoList.text.push(newtodo);
+      },
+
+      removeToDo(i) {
+        this.todoList.splice(i, 1);
+      },
+
+      change(i) {
+        this.todoList[i].done = ! this.todoList[i].done;
+      }
+  },
+
+
+
+
+
+}).mount('#app')
